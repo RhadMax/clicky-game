89853f4e1677c79c452d7ef5edc38747c4089b09
Max Patten <amanthus2535@gmail.com>
2019-07-24 20:06:53 -0700

    added images, set up basic page structure, started writing helper functions and methods for app class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import images from "./images.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const clickedImages = [];
+
+const isClicked = imageId => {
+
+}
+
+const randomlySortImages = arr => arr.sort(() => Math.random() - 0.5);
+
+class App extends React.Component {
+  state = {
+    score: 0,
+    topScore: 0
+  }
+
+  handleClick = id => {
+
+  };
+
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-light bg-light">
+          <span className="navbar-brand mb-0 h1">Clicky Game</span>
+          <span className="navbar-brand mb-0 h3">Score: {this.state.score}</span>
+          <span className="navbar-brand mb-0 h3">Top Score: {this.state.topScore}</span>
+        </nav>
+        <div className="container">
+          <div className="row">
+            {images.map(image =>
+              <a onClick={() => this.handleClick(image.id)} className="col-sm-3">
+                <div className="card">
+                  <img src={process.env.PUBLIC_URL + "/img/" + image.relPath} />
+                </div>
+              </a>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
