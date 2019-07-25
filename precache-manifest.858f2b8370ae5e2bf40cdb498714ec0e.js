b3ceb0d372e388c2e91f655899c8fd78114b38cb
Max Patten <amanthus2535@gmail.com>
2019-07-24 23:16:07 -0700

    Updates

---
+++ precache-manifest.858f2b8370ae5e2bf40cdb498714ec0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca410f7f7cb8972ae9d1d9508f80114",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "bb1280de69f1ffd7b7a2",
+    "url": "/clicky-game/static/css/main.b544b4e3.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "bb1280de69f1ffd7b7a2",
+    "url": "/clicky-game/static/js/main.8a154250.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
